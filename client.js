d672db0aaa58769f8e32c088ed0b52a02973b383
Otto Hyytiälä <otto.hyytiala@gmail.com>
2019-05-15 21:00:30 +0300

    use camping site 360 images

---
+++ client.js
@@ -0,0 +1,29 @@
+
+import {ReactInstance} from 'react-360-web';
+
+function init(bundle, parent, options = {}) {
+  const r360 = new ReactInstance(bundle, parent, {
+    fullScreen: true,
+    ...options,
+  });
+
+  r360.renderToSurface(
+    r360.createRoot('kamppa', {
+      photos: [
+        {uri: './static_assets/piha_1.jpg', title: 'Kämpän pihapiiri', format: '2D'},
+        {uri: './static_assets/eteinen.jpg', title: 'Kämpän eteinen', format: '2D'},
+        {uri: './static_assets/tupa_1.jpg', title: 'Kämpän tupa 1', format: '2D'},
+        {uri: './static_assets/tupa_2.jpg', title: 'Kämpän tupa 2', format: '2D'},
+        {uri: './static_assets/keittio.jpg', title: 'Kämpän keittiö', format: '2D'},
+        {uri: './static_assets/pikkukamppa.jpg', title: 'Kämpän pikkukämppä', format: '2D'},
+        {uri: './static_assets/sauna_tupa.jpg', title: 'Saunan tupa', format: '2D'},
+        {uri: './static_assets/pesutupa.jpg', title: 'Saunan pesuhuone', format: '2D'},
+        {uri: './static_assets/sauna.jpg', title: 'Sauna', format: '2D'},
+        {uri: './static_assets/piha_2.jpg', title: 'Puuliiteri & hakkuupaikka', format: '2D'},
+      ],
+    }),
+    r360.getDefaultSurface(),
+  );
+}
+
+window.React360 = {init};
